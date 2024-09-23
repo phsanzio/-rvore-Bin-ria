5c8eb184fff986eb0d0a86086680a521c92ee84b
phsanzio <phsanzio@outlook.com>
2024-09-23 19:44:34 -0300

    start binary tree

---
+++ Node.java
@@ -0,0 +1,35 @@
+public class Node {
+    private int value;
+    private Node right;
+    private Node left;
+
+    public Node(int value){
+        this.value = value;
+        this.right = null;
+        this.left = null;
+    }
+
+    public int getValue(){
+        return value;
+    }
+
+    public void setValue(int value){
+        this.value = value;
+    }
+
+    public Node getRight() {
+        return right;
+    }
+
+    public Node getLeft() {
+        return left;
+    }
+
+    public void setRight(Node right) {
+        this.right = right;
+    }
+
+    public void setLeft(Node left) {
+        this.left = left;
+    }
+}
---
+++ Tree.java
@@ -0,0 +1,66 @@
+public class Tree {
+    private Node root;
+
+    public Tree(){
+        this.root = null;
+    }
+
+    public Node getRoot() {
+        return root;
+    }
+
+    public void setRoot(Node root) {
+        this.root = root;
+    }
+
+    public boolean empty(){
+        return root == null;
+    }
+
+    private Node insertNew(Node newNode, Node current){
+        if (current == null) {
+            return newNode;
+        }
+        if (current.getValue() > newNode.getValue()){
+            current.setLeft(insertNew(newNode, current.getRight()));
+        } else {
+            current.setRight(insertNew(newNode, current.getRight()));
+        }
+        return current;
+    }
+
+    public void Insert(Node newNode){
+        root = insertNew(newNode, root);
+    }
+
+    public void printPreOrder(Node newNode){
+        if (newNode != null) {
+            System.out.println(newNode.getValue() + "\n");
+            printPreOrder(newNode.getLeft());
+            printPreOrder(newNode.getRight());
+        }
+    }
+
+    private Node findNode(Node current, int v){
+        if (current == null) {
+            return null;
+        }
+        if (current.getRight() != null) {
+            if (current.getRight().getValue() == v) {
+                return current;
+            }
+        }
+
+        if (current.getLeft() != null) {
+            if (current.getLeft().getValue() == v) {
+                return current;
+            }
+        }
+
+        if (current.getValue() > v) {
+            return findNode(current.getRight(), v);
+        } else {
+            return findNode(current.getLeft(), v);
+        }
+    }
+}
